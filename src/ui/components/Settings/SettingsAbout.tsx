ca1c3e268835ebe35986d461d8e124f92d84bfad
Eliyahu Georgios <eli.georgios@gmail.com>
2018-09-12 13:28:44 -0400

    Customized About Settings file (Changed Museeks to Ravenode)
    Signed-off-by: Eliyahu Georgios <eli.georgios@gmail.com>

--- src/ui/components/Settings/SettingsAbout.tsx
+++ src/ui/components/Settings/SettingsAbout.tsx
@@ -17,37 +17,41 @@ export default class SettingsAbout extends React.Component {
     return (
       <div className='setting setting-about'>
         <div className='setting-section'>
-          <h4>About Museeks</h4>
-          <p>
-            Museeks { app.version }{ ' - ' }
-            <ExternalLink href='http://museeks.io'>museeks.io</ExternalLink>
+          <h4>About Ravenode</h4>
+          <p>Ravenode was forked from Museeks.io on September 10, 2018.</p>
+          <p>Ravenode { app.version }{ ' - ' }
+            <ExternalLink href='http://github.com/eligeorgios/ravenode'>Ravenode Repository</ExternalLink>{ ' - ' }<ExternalLink href='http://museeks.io'>Museeks.io</ExternalLink>
+
             { ' - ' }
             <ExternalLink href={`https://github.com/KeitIG/Museeks/releases/tag/$
-            {app.version}`}>release notes</ExternalLink>
+            {app.version}`}>Release Notes</ExternalLink>
           </p>
           <Button
             bsSize='small' className='update-checker'
             onClick={async () => { await SettingsActions.checkForUpdate(); }}
-          >Check for update</Button>
+          >Check for updates</Button>
+        
         </div>
+        <br />
         <div className='setting-section'>
           <h4>Contributors</h4>
           <div className='contributors-list'>
             <p>
-              Made with <span className='heart'>♥</span> by Pierre de la Martinière
-              (<ExternalLink href='http://pierrevanmart.com'>KeitIG</ExternalLink>)
-              and a bunch of <ExternalLink href='https://github.com/KeitIG/museeks/graphs/contributors'>great people</ExternalLink>.
+              Made with lots of <span className='heart'>♥</span> by Eliyahu Georgios (<ExternalLink href='http://github.com/eligeorgios/'>eligeorgios</ExternalLink>), Pierre de la Martinière
+              (<ExternalLink href='http://pierrevanmart.com'>KeitIG</ExternalLink>),
+              and a bunch of <ExternalLink href='https://github.com/KeitIG/museeks/graphs/contributors'>great developers over at Github.</ExternalLink>.
             </p>
+           <br />
           </div>
         </div>
         <div className='setting-section'>
           <h4>Report issue / Ask for a feature</h4>
           <div className='contributors-list'>
             <p>
-              Although Museeks is mostly stable, a few bugs may still occur. Please, do
-              not hesitate to report them or to ask for features you would like to
-              see, using our <ExternalLink href='http://github.com/KeitIG/Museeks/issues'>issue tracker</ExternalLink>.
+              Although Ravenode/Museeks is mostly stable, there are still a few bugs buried somewhere in the haystack. Don't hesitate to report them or to ask for features you would like
+               using our <ExternalLink href='http://github.com/eligeorgios/ravenode/issues'>bug tracker</ExternalLink>.
             </p>
+         <br />
           </div>
         </div>
       </div>
